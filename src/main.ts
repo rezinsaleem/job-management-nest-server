bece6e9b2489572716cffa7c077f32339cad6031
rezinsaleem <rezinsaleem1@gmail.com>
2025-05-29 15:27:00 +0530

    frontend-url added

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,11 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors({
+  origin: ['https://job-management-next-app.vercel.app/'], // update this
+  credentials: true,
+});
   await app.listen(process.env.PORT ?? 5000);
 }
 bootstrap();
+
